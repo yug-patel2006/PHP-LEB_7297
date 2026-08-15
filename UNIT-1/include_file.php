4d800d696f2c0dc0b1ee1a85026c06e4c3ecbbfc
yug-patel2006 <yugp4040@gmail.com>
2026-08-15 16:52:53 +0530

    Create include_file.php

---
+++ UNIT-1/include_file.php
@@ -0,0 +1,6 @@
+<?php
+// This file is included/required by 1.7_include_require.php
+echo "This message is coming from include_file.php.<br>";
+echo "It was loaded using include/require.<br>";
+$sharedMessage = "Hello, I am a variable from the included file!";
+?>
